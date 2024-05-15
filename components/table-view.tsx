5556fe17dde3478c3d9f09f1e725ecf05fb48b77
Son Le <ayzkeys@gmail.com>
2024-05-15 22:15:37 +0700

    dnd prep

--- components/table-view.tsx
+++ components/table-view.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, useState, CSSProperties } from "react";
 import { Box } from "@chakra-ui/react";
 import {
   useReactTable,
@@ -9,7 +9,31 @@ import {
   ColumnDef,
   SortingState,
   getSortedRowModel,
+  Header,
+  Cell,
 } from "@tanstack/react-table";
+
+// needed for table body level scope DnD setup
+import {
+  DndContext,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  closestCenter,
+  type DragEndEvent,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { restrictToHorizontalAxis } from "@dnd-kit/modifiers";
+import {
+  arrayMove,
+  SortableContext,
+  horizontalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+// needed for row & cell level scope DnD setup
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface TableViewProps {
   columns: ColumnDef<any, any>[];
@@ -18,18 +42,86 @@ interface TableViewProps {
 
 const TableView: FC<TableViewProps> = ({ columns, data }) => {
   const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnOrder, setColumnOrder] = useState<string[]>(() =>
+    columns.map((c) => c.id!)
+  );
 
   const table = useReactTable({
     data,
     columns,
-    state: { sorting },
+    state: { sorting, columnOrder },
     getCoreRowModel: getCoreRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
     columnResizeMode: "onChange",
   });
 
-  console.log(data);
+  const DraggableTableHeader = ({
+    header,
+  }: {
+    header: Header<unknown, unknown>;
+  }) => {
+    const { attributes, isDragging, listeners, setNodeRef, transform } =
+      useSortable({
+        id: header.column.id,
+      });
+
+    const style: CSSProperties = {
+      opacity: isDragging ? 0.8 : 1,
+      position: "relative",
+      transform: CSS.Translate.toString(transform), // translate instead of transform to avoid squishing
+      transition: "width transform 0.2s ease-in-out",
+      whiteSpace: "nowrap",
+      width: header.column.getSize(),
+      zIndex: isDragging ? 1 : 0,
+    };
+
+    return (
+      <th colSpan={header.colSpan} ref={setNodeRef} style={style}>
+        {header.isPlaceholder
+          ? null
+          : flexRender(header.column.columnDef.header, header.getContext())}
+        <button {...attributes} {...listeners}>
+          🟰
+        </button>
+      </th>
+    );
+  };
+
+  const DragAlongCell = ({ cell }: { cell: Cell<unknown, unknown> }) => {
+    const { isDragging, setNodeRef, transform } = useSortable({
+      id: cell.column.id,
+    });
+
+    const style: CSSProperties = {
+      opacity: isDragging ? 0.8 : 1,
+      position: "relative",
+      transform: CSS.Translate.toString(transform), // translate instead of transform to avoid squishing
+      transition: "width transform 0.2s ease-in-out",
+      width: cell.column.getSize(),
+      zIndex: isDragging ? 1 : 0,
+    };
+
+    return (
+      <td style={style} ref={setNodeRef}>
+        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+      </td>
+    );
+  };
+
+  // reorder columns after drag & drop
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (active && over && active.id !== over.id) {
+      setColumnOrder((columnOrder) => {
+        const oldIndex = columnOrder.indexOf(active.id as string);
+        const newIndex = columnOrder.indexOf(over.id as string);
+        return arrayMove(columnOrder, oldIndex, newIndex); //this is just a splice util
+      });
+    }
+  }
+
+  const sensors = useSensors(useSensor(MouseSensor, {}));
 
   return (
     <>
@@ -55,49 +147,56 @@ const TableView: FC<TableViewProps> = ({ columns, data }) => {
           ))}
         </Box>
       </Box>
-      <table
-        className="min-w-full border border-gray-300 divide-y divide-gray-200"
-        style={{ width: table.getTotalSize() }}
+      <DndContext
+        collisionDetection={closestCenter}
+        modifiers={[restrictToHorizontalAxis]}
+        onDragEnd={handleDragEnd}
+        sensors={sensors}
       >
-        <thead className="">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  onClick={header.column.getToggleSortingHandler()}
-                  className="relative px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  style={{ width: header.getSize() }}
-                >
-                  {header.column.columnDef.header?.toString() ?? null}
-                  {{
-                    asc: " 🔼",
-                    desc: " 🔽",
-                  }[header.column.getIsSorted() as string] ?? null}
-                  <div
-                    onMouseDown={header.getResizeHandler()}
-                    onTouchStart={header.getResizeHandler()}
-                    className={`resizer ${
-                      header.column.getIsResizing() ? "isResizing" : ""
-                    }`}
-                  />
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className="px-6 py-4 whitespace-nowrap">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <table
+          className="min-w-full border border-gray-300 divide-y divide-gray-200"
+          style={{ width: table.getTotalSize() }}
+        >
+          <thead className="">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    onClick={header.column.getToggleSortingHandler()}
+                    className="relative px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    style={{ width: header.getSize() }}
+                  >
+                    {header.column.columnDef.header?.toString() ?? null}
+                    {{
+                      asc: " 🔼",
+                      desc: " 🔽",
+                    }[header.column.getIsSorted() as string] ?? null}
+                    <div
+                      onMouseDown={header.getResizeHandler()}
+                      onTouchStart={header.getResizeHandler()}
+                      className={`resizer ${
+                        header.column.getIsResizing() ? "isResizing" : ""
+                      }`}
+                    />
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="px-6 py-4 whitespace-nowrap">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </DndContext>
     </>
   );
 };
